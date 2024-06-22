32b0351c0b59184518a6695be4bdc52a136a379c
vemurikethan <kethan.vemuri-ee@infineon.com>
2024-06-22 21:22:08 +0530

    Initial Commit

---
+++ frontend/src/main.js
@@ -0,0 +1,21 @@
+import { createApp } from 'vue'
+import { createPinia } from 'pinia'
+
+import App from './App.vue'
+import router from './router'
+import PrimeVue from 'primevue/config'
+import Aura from '@primevue/themes/aura'
+
+import '@/assets/styles.scss'
+
+const app = createApp(App)
+
+app.use(createPinia())
+app.use(router)
+app.use(PrimeVue, {
+  theme: {
+    preset: Aura
+  }
+})
+
+app.mount('#app')
---
+++ frontend/src/views/HomeView.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <h1>Home</h1>
+    <p>This is the Home page</p>
+    <Checkbox v-model="checked" :binary="true" />
+
+    <Button label="Submit" />
+  </div>
+</template>
+<script setup>
+import Checkbox from 'primevue/checkbox'
+import Button from 'primevue/button'
+
+import { ref } from 'vue'
+
+const checked = ref(false)
+</script>
